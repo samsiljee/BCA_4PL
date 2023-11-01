674f375968d21e31ace622720a41ffc6ffaa42c1
samsiljee <samsiljee@gmail.com>
2023-11-01 18:23:53 +1300

    Edited further to get it running

--- 4PL.R
+++ 4PL.R
@@ -2,17 +2,26 @@
 # Sam Siljee
 # Created 1st November 2023
 
+# Load libraries
+library(drc)
+
+# Load data
+dat <- read.csv("standards.csv")
+
 # Fit 4PL curve
-fit <- drm(responses ~ concentrations, data = your_data_frame, fct = LL.4())
+fit <- drm(absorbance ~ concentration, data = dat, fct = LL.4())
 
 # View the summary of the fitted curve
 summary(fit)
 
+# Generate a vector of some random test data
+test_dat <- c("Sample_A" = 0.5667, "Sample_B" = 1.004, "Sample_c" = 0.2332)
+
 # Predict concentrations of samples
-predicted_concentrations <- predict(fit, newdata = data.frame(responses = sample_responses))
+predicted_concentrations <- predict(fit, newdata = data.frame(responses = test_dat))
 
 # Plot the fitted curve
-plot(fit, log = "x", col = "red", xlab = "Concentration", ylab = "Response")
+plot(fit, log = "x", col = "red", xlab = "Concentration", ylab = "Absorbance")
 
 # Add original data points to the plot
-points(concentrations, responses, pch = 16, col = "black")
+points(dat$concentration, dat$absorbance, pch = 16, col = "black")
