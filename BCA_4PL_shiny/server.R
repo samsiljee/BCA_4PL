9fb073ef1cc34c953c7eb6bbc829c5db14d5f0b6
samsiljee <samsiljee@gmail.com>
2023-11-16 10:48:21 +1300

    Long metadata now started - not yet working for duplicates across rows

--- BCA_4PL_shiny/server.R
+++ BCA_4PL_shiny/server.R
@@ -3,40 +3,41 @@
 # Created 30 October 2023
 
 library(shiny)
-library(stringr)
-library(ggplot2)
-library(dplyr)
+library(tidyverse)
 library(wesanderson)
 
-colour_pallet <- wes_palettes$AsteroidCity3[c(4,3,1,2)]
+colour_pallet <- wes_palettes$AsteroidCity3[c(4, 3, 1, 2)]
 
 server <- function(input, output, session) {
+  # Display annotations to test
+  output$test_1 <- renderPrint(print(metadata_long(), n = 100))
+  output$test_2 <- renderPrint(print(absorbance_long(), n = 100))
 
   # Annotations ----
-  
+
   # Create a data frame with individual coordinates
   coordinates <- reactive({
     switch(input$direction,
-           "columns" = {
-             df <- expand.grid("rows" = 1:nrow(blank_matrix()), "cols" = 1:ncol(blank_matrix()))
-             df$Index <- 1:nrow(df)
-             df
-           },
-           "rows" = {
-             df <- expand.grid("cols" = 1:ncol(blank_matrix()), "rows" = 1:nrow(blank_matrix()))
-             df$Index <- 1:nrow(df)
-             df
-           }
+      "columns" = {
+        df <- expand.grid("rows" = 1:nrow(blank_matrix()), "cols" = 1:ncol(blank_matrix()))
+        df$Index <- 1:nrow(df)
+        df
+      },
+      "rows" = {
+        df <- expand.grid("cols" = 1:ncol(blank_matrix()), "rows" = 1:nrow(blank_matrix()))
+        df$Index <- 1:nrow(df)
+        df
+      }
     )
   })
-  
+
   # Initialise blank dataframe for grid input
   annotations <- reactiveVal(data.frame())
-  
+
   # Create a dataframe reading in the values of the grid input
   annotations <- reactive({
     data <- data.frame()
-    
+
     # Loop through the number of rows in coordinates
     for (index in 1:nrow(coordinates())) {
       current_row <- data.frame(
@@ -45,35 +46,84 @@ server <- function(input, output, session) {
         Name = input[[paste0("name_", index)]],
         Concentration = input[[paste0("concentration_", index)]]
       )
-      
+
       data <- rbind(data, current_row)
     }
-    
+
     # Return the dataframe
     return(data)
   })
-  
+
   # Combine annotations with coordinates to make metadata table
   metadata <- reactive({
     df <- merge(annotations(), coordinates(), by = "Index")
-    
+
     # Generate labels, rows names, and col names
     df <- df %>%
       group_by(Type) %>%
       mutate(Label = paste(Type, row_number())) %>%
       ungroup() %>%
-      mutate(Row_name = rownames(blank_matrix()[rows,]),
-             Col_name = colnames(blank_matrix()[,cols]))
-    
+      mutate(
+        Row_name = rownames(blank_matrix()[rows, ]),
+        Col_name = colnames(blank_matrix()[, cols])
+      )
+
     df
   })
-  
-  # Display annotations to test
-  output$test_1 <- renderPrint(print(metadata(), n = 100))
-  output$test_2 <- renderPrint(blank_matrix())
+
+  # Make a long version of metadata to work with long absorbance data
+  metadata_long <- reactive({
+    if (input$replicates == "1") { # Return unchanged metadata table if not replicated
+      df <- metadata()
+    } else { # Handle case for missing rows if experiment done in triplicate with replicates across rows
+      if (input$replicates == "3" & input$direction == "rows") {
+        data.frame(1:3, 1:3)
+      } else {
+        # Replicate data.frame for replicates
+        df <- switch(input$replicates,
+          "1" = metadata(),
+          "2" = rbind(metadata(), metadata()),
+          "3" = rbind(metadata(), metadata(), metadata())
+        )
+        # Sort by index
+        df <- arrange(df, Index)
+        # Add columns and rows as needed
+        if (input$direction == "columns") {
+          df <- df %>%
+            mutate(Col_name = switch(
+              input$replicates,
+              "2" = c(
+                rep(1:2, 8),
+                rep(3:4, 8),
+                rep(5:6, 8),
+                rep(7:8, 8),
+                rep(9:10, 8),
+                rep(11:12, 8)),
+              "3" = c(
+                rep(1:3, 8),
+                rep(4:6, 8),
+                rep(7:9, 8),
+                rep(10:12, 8))
+            )
+            )
+        } else {
+          df <- df %>%
+            mutate(Row_name = c(
+              rep(LETTERS[1:2], 12),
+              rep(LETTERS[3:4], 12),
+              rep(LETTERS[5:6], 12),
+              rep(LETTERS[7:8], 12)
+            )
+            )
+        }
+        
+        df
+      }
+    }
+  })
   
   # Plate plan plot ----
-  
+
   # Make a matrix to display plate plan based on replication parameters
   blank_matrix <- reactive({
     if (input$direction == "columns") { # Make matrix for duplicates across columns
@@ -93,14 +143,14 @@ server <- function(input, output, session) {
       blank_mat <- matrix(NA, 8 / as.numeric(input$replicates), 12)
       colnames(blank_mat) <- 1:12
       rownames(blank_mat) <- switch(input$replicates,
-                                    "1" = LETTERS[1:8],
-                                    "2" = c("A - B", "C - D", "E - F", "G - H"),
-                                    "3" = c("A - C", "D - F")
+        "1" = LETTERS[1:8],
+        "2" = c("A - B", "C - D", "E - F", "G - H"),
+        "3" = c("A - C", "D - F")
       )
       blank_mat
     }
   })
-  
+
   # Plot to display plate plan
   output$plate_plan_plot <- renderPlot({
     ggplot(metadata(), aes(x = cols, y = rows, label = Label, color = Type)) +
@@ -124,11 +174,11 @@ server <- function(input, output, session) {
   })
 
   # Reactive UI ----
-  
+
   test_vector <- reactive({
     1:nrow(metadata())
   })
-  
+
   # Create grid of UI elements for input
   output$grid_input <- renderUI({
     inputs <- lapply(1:nrow(coordinates()), function(index) {
@@ -156,7 +206,7 @@ server <- function(input, output, session) {
   })
 
   # Input ----
-  
+
   # Load raw absorbance data from text input
   absorbance <- reactive({
     # Initialise blank data.frame
@@ -184,6 +234,20 @@ server <- function(input, output, session) {
 
     # Return the table
     return(dat)
+  })
+
+  # Function to reshape absorbance data
+  reshape_absorbance <- function(dat, direction, replicates) {
+    dat_long <- dat %>%
+      as_tibble(rownames = "Row") %>%
+      pivot_longer(cols = -Row, names_to = "Column", values_to = "Absorbance")
+
+    return(dat_long)
+  }
+
+  # Reactive to reshape absorbance data
+  absorbance_long <- reactive({
+    reshape_absorbance(absorbance(), input$direction, input$replicates)
   })
 
   # Render raw absorbance data table
